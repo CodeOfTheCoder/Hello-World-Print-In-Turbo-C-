8f22b8876ff22cdc367240bb74da0b5f08652016
CodeOfTheCoder <48353211+CodeOfTheCoder@users.noreply.github.com>
2019-03-09 03:53:58 -0800

    Add files via upload

---
+++ HELLOWORLD.C
@@ -0,0 +1,18 @@
+//First program of C Language
+//How to print Hello World..
+
+//First Include Header File
+#include<stdio.h>//Stander Input Output.Header
+#include<conio.h>//Consol Input Output.Header
+//This Header Files are Preprocessor Commands
+
+
+void main()//Main Function
+{
+	clrscr();//Clear screen...clrscr() Linked with <conio.h>
+	printf("Hello World");//Printf Linked with <stdio.h>
+	printf("\n Hiii");
+	getch();//getch()...Linked with <conio.h>
+}
+//Thanks for watching
+//Please Like Share and Subscribe our Channel...
